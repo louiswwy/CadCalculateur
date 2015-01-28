47d3e154b7f9c43c304b01791dc8c16451ce257c
猜猜看 <louis664724128@hotmail.com>
2015-01-28 17:35:53 +0800

    1-28

--- ClassLibrary1/CalculerLine.cs
+++ ClassLibrary1/CalculerLine.cs
@@ -14,7 +14,7 @@ namespace Calculers
 {
     public class CalculerLine
     {
-        public List<Entity> listEntitys
+        public List<Line> listEntitys
         {
             get;
             set;
@@ -27,40 +27,49 @@ namespace Calculers
             
             Document acDoc = Application.DocumentManager.MdiActiveDocument;
             Database acCurDb = acDoc.Database;
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
             // Start a transaction
             using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
             {
-                // Request for objects to be selected in the drawing area
-                PromptSelectionResult acSSPrompt = acDoc.Editor.GetSelection();
-                // If the prompt status is OK, objects were selected
-                if (acSSPrompt.Status == PromptStatus.OK)
-                {
-                    SelectionSet acSSet = acSSPrompt.Value;
-                    // Step through the objects in the selection set
-                    foreach (SelectedObject acSSObj in acSSet)
-                    {
-                        // Check to make sure a valid SelectedObject object was returned
-                        if (acSSObj != null)
-                        {
-                            // Open the selected object for write
-                            Line acEnt = acTrans.GetObject(acSSObj.ObjectId,
-                                                   OpenMode.ForWrite) as Line;
 
-                            listEntitys.Add(acEnt);
-                            if (acEnt != null)
+                try
+                {
+                    // Request for objects to be selected in the drawing area
+                    PromptSelectionResult acSSPrompt = acDoc.Editor.GetSelection();
+                    // If the prompt status is OK, objects were selected
+                    if (acSSPrompt.Status == PromptStatus.OK)
+                    {
+                        SelectionSet acSSet = acSSPrompt.Value;
+                        // Step through the objects in the selection set
+                        foreach (SelectedObject acSSObj in acSSet)
+                        {
+                            // Check to make sure a valid SelectedObject object was returned
+                            if (acSSObj != null)
                             {
-                                // Change the object's color to Green
-                                acEnt.ColorIndex = 3;
+                                // Open the selected object for write
+                                Line acEnt = acTrans.GetObject(acSSObj.ObjectId,
+                                                       OpenMode.ForRead) as Line;
+
+                                //listEntitys.Add(acEnt);  //导致cad程序崩溃.
+                                //if (acEnt != null)
+                                //{
+                                    // Change the object's color to Green
+                                //    acEnt.ColorIndex = 3;
+                                //}
                             }
                         }
+                        // Save the new object to the database
+                        acTrans.Commit();
                     }
-                    // Save the new object to the database
-                    acTrans.Commit();
+                }
+                catch (Autodesk.AutoCAD.Runtime.Exception ex)
+                {
+                    ed.WriteMessage(ex.Message + "\n");
                 }
             }
             MainForm form = new MainForm();     //创建对话框
             Application.ShowModalDialog(form);  //显示模态对话框
-            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            
         }
     }
 }
